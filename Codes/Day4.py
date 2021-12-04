25c9125c370ef7b9ce824e2ee3d10ec182cb0bae
Agasayan <matfizgiz@gmail.com>
2021-12-04 15:02:06 +0100

    commit day4 done

---
+++ Codes/Day4.py
@@ -0,0 +1,152 @@
+import numpy as np
+
+from Library import *
+
+
+class BingoPartOne:
+    def __init__(self, boards, board_size):
+        self.boards = np.array(boards)
+        self.number_of_boards = len(boards)
+        self.board_size = board_size
+        self.chosen_numbers = np.zeros(shape=(len(self.boards), self.board_size, self.board_size))
+        self.rows_scores = np.zeros(shape=(len(self.boards), self.board_size))
+        self.columns_scores = np.zeros(shape=(len(self.boards), self.board_size))
+
+    def __repr__(self):
+        return "BOARDS:\n" + str(self.boards) + '\n\n' + "CHOSEN NUMBERS:\n" + \
+               str(self.chosen_numbers) + '\n\n' + "ROW SCORES:\n" + \
+               str(self.rows_scores) + '\n\n' + "COLUMNS SCORES:\n" + \
+               str(self.columns_scores)
+
+    def check_winner(self, i):
+        results_rows = np.where(self.rows_scores[i] == self.board_size)
+        results_columns = np.where(self.columns_scores[i] == self.board_size)
+        if len(results_rows[0]) != 0:
+            return True
+        if len(results_columns[0]) != 0:
+            return True
+        return False
+
+    def calculate_board_score(self, i, x):
+        """
+
+        :param i: number of the board
+        :param x: called number
+        :return:
+        """
+        sum_of_uncalled_numbers = np.sum(np.where(self.chosen_numbers[i] == 0, self.boards[i], 0))
+        return sum_of_uncalled_numbers * x
+
+    def add_number_to_board(self, x, board_number):
+        """
+
+        :param x: number to add
+        :param board_number: number of board
+        :return:
+        """
+        for i in range(self.board_size):
+            for j in range(self.board_size):
+                if self.boards[board_number][i][j] == x:
+                    if self.chosen_numbers[(board_number, i, j)] == 0:
+                        self.chosen_numbers[(board_number, i, j)] = 1
+                        self.rows_scores[(board_number, i)] += 1
+                        self.columns_scores[(board_number, j)] += 1
+
+    def add_number_to_all_boards(self, x):
+        for i in range(len(self.boards)):
+            self.add_number_to_board(x, i)
+            if self.check_winner(i):
+                print(self.calculate_board_score(i, x))
+                return True
+        return False
+
+
+class BingoPartTwo:
+    def __init__(self, boards, board_size):
+        self.boards = np.array(boards)
+        self.number_of_boards = len(boards)
+        self.board_size = board_size
+        self.chosen_numbers = np.zeros(shape=(len(self.boards), self.board_size, self.board_size))
+        self.rows_scores = np.zeros(shape=(len(self.boards), self.board_size))
+        self.columns_scores = np.zeros(shape=(len(self.boards), self.board_size))
+        self.winners = np.array([0] * len(self.boards))
+
+    def __repr__(self):
+        return "BOARDS:\n" + str(self.boards) + '\n\n' + "CHOSEN NUMBERS:\n" + \
+               str(self.chosen_numbers) + '\n\n' + "ROW SCORES:\n" + \
+               str(self.rows_scores) + '\n\n' + "COLUMNS SCORES:\n" + \
+               str(self.columns_scores) + '\n\n' + "WINNERS:\n" + \
+               str(self.winners)
+
+    def check_winner(self, i):
+        results_rows = np.where(self.rows_scores[i] == self.board_size)
+        results_columns = np.where(self.columns_scores[i] == self.board_size)
+        if len(results_rows[0]) != 0:
+            return True
+        if len(results_columns[0]) != 0:
+            return True
+        return False
+
+    def calculate_board_score(self, i, x):
+        """
+
+        :param i: number of the board
+        :param x: called number
+        :return:
+        """
+        sum_of_uncalled_numbers = np.sum(np.where(self.chosen_numbers[i] == 0, self.boards[i], 0))
+        return sum_of_uncalled_numbers * x
+
+    def add_number_to_board(self, x, board_number):
+        """
+
+        :param x: number to add
+        :param board_number: number of board
+        :return:
+        """
+        for i in range(self.board_size):
+            for j in range(self.board_size):
+                if self.boards[board_number][i][j] == x:
+                    if self.chosen_numbers[(board_number, i, j)] == 0:
+                        self.chosen_numbers[(board_number, i, j)] = 1
+                        self.rows_scores[(board_number, i)] += 1
+                        self.columns_scores[(board_number, j)] += 1
+
+    def add_number_to_all_boards(self, x):
+        for i in range(len(self.boards)):
+            self.add_number_to_board(x, i)
+            if self.check_winner(i):
+                self.winners[i] = 1
+                if np.sum(self.winners) == len(self.boards):
+                    print(self.calculate_board_score(i, x))
+                    return True
+        return False
+
+
+# ----------------------------------------------------------------------------------------------------------------------
+# --------------------------------------------- PART ONE ---------------------------------------------------------------
+# ----------------------------------------------------------------------------------------------------------------------
+def part_one():
+    parser = Day4Parser('input4.txt')
+    parser.parse_numbers()
+    parser.parse_boards()
+    game_board = BingoPartOne(parser.boards, parser.board_size)
+    for i in range(len(parser.numbers)):
+        if game_board.add_number_to_all_boards(parser.numbers[i]):
+            break
+
+
+# ----------------------------------------------------------------------------------------------------------------------
+# --------------------------------------------- PART TWO ---------------------------------------------------------------
+# ----------------------------------------------------------------------------------------------------------------------
+def part_two():
+    parser = Day4Parser('input4.txt')
+    parser.parse_numbers()
+    parser.parse_boards()
+    game_board = BingoPartTwo(parser.boards, parser.board_size)
+    for i in range(len(parser.numbers)):
+        if game_board.add_number_to_all_boards(parser.numbers[i]):
+            break
+
+
+part_two()
--- Library.py
+++ Library.py
@@ -1,3 +1,36 @@
 PATH = "/home/hubert/PycharmProjects/AdventOfCode2021"
 PATH_TO_CODES = PATH + "/Codes"
 PATH_TO_INPUTS = PATH + "/Inputs"
+
+
+class Day4Parser:
+    def __init__(self, file):
+        with open(PATH_TO_INPUTS + '/' + file, 'r') as f:
+            self.data = f.read()
+        self.data = self.data.replace('\n\n', ';')
+        self.numbers = None
+        self.boards = None
+        self.board_size = 5
+
+    def parse_numbers(self):
+        i = 0
+        while self.data[i] != ';':
+            i += 1
+        self.numbers = self.data[:i].split(',')
+        self.numbers = list(map(int, self.numbers))
+        self.data = self.data[i + 1:]
+
+    def parse_boards(self):
+        N = self.data.count(';') + 1
+        self.boards = [[[0 for _ in range(self.board_size)] for _ in range(self.board_size)] for _ in range(N)]
+        self.data = self.data.split(';')
+        self.data = [board.replace('\n', ' ').replace('   ', ' ').replace('  ', ' ') for board in self.data]
+        self.data = [board.split(' ') for board in self.data]
+        for i in range(N):
+            if self.data[i][0] == '':
+                self.data[i].pop(0)
+        self.data = [list(map(int, board)) for board in self.data]
+        for n in range(N):
+            for i in range(self.board_size):
+                for j in range(self.board_size):
+                    self.boards[n][i][j] = self.data[n][i * 5 + j]
